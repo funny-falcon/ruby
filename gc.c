ac969bf65a0ff5f757df304e15e16dba27be4924
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-12-20 10:01:14 +0000

    class local method cache

--- gc.c
+++ gc.c
@@ -1539,6 +1539,7 @@ obj_free(rb_objspace_t *objspace, VALUE obj)
 	}
 	rb_class_remove_from_module_subclasses(obj);
 	rb_class_remove_from_super_subclasses(obj);
+	rb_method_cache_clear(obj);
 	if (RANY(obj)->as.klass.ptr)
 	    xfree(RANY(obj)->as.klass.ptr);
 	RANY(obj)->as.klass.ptr = NULL;
@@ -1609,6 +1610,7 @@ obj_free(rb_objspace_t *objspace, VALUE obj)
 	}
 	rb_class_remove_from_module_subclasses(obj);
 	rb_class_remove_from_super_subclasses(obj);
+	rb_method_cache_clear(obj);
 	xfree(RANY(obj)->as.klass.ptr);
 	RANY(obj)->as.klass.ptr = NULL;
 	break;
